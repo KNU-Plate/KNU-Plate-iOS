0869c3d95754a777b6e20c8cd9bb1f240f4443a2
김영채 <kevinkim2586@gmail.com>
2021-03-22 23:19:23 +0900

    Upload initial project file

---
+++ KNU_Plate_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KNU_Plate_iOS
+//
+//  Created by Kevin Kim on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
