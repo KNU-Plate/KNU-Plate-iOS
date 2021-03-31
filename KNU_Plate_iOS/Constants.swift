f7d12af9090908692c212a46e4b5c024083969eb
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-03-31 23:00:53 +0900

    Little UI Changes

--- KNU_Plate_iOS/Constants.swift
+++ KNU_Plate_iOS/Constants.swift
@@ -10,6 +10,12 @@ struct Constants {
         static let restaurantCollectionViewController = "RestaurantCollectionViewController"
     }
     
+    struct Layer {
+        static let borderWidth: CGFloat = 1
+        static let cornerRadius: CGFloat = 15.0
+        static let borderColor: CGColor = UIColor.lightGray.cgColor
+    }
+    
     static let gateNames: [String] = ["북문", "정/쪽문", "동문", "서문"]
     
     static let heightPerWidthRestaurantCell: CGFloat = 1.1
