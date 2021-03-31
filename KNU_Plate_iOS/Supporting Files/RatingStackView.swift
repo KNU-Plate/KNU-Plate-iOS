529115da3d1dbcbe3812ad65e45a4552e064f26f
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-03-31 14:54:16 +0900

    Create RatingStackView, delete JinRatingController

---
+++ KNU_Plate_iOS/Supporting Files/RatingStackView.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+/// Shows stars rating using ImageView
+class RatingStackView: UIStackView {
+    var starImages: [UIImageView] = []
+    let starsEmptyPicName = "star"                  // Empty star name (SF Symbol)
+    let starsFilledPicName = "star.fill"            // Filled star name (SF Symbol)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.axis = .horizontal
+        self.alignment = .fill
+        self.distribution = .fillEqually
+        for _ in 0..<5 {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFit
+            imageView.tintColor = .systemYellow
+            imageView.image = UIImage(systemName: starsEmptyPicName)
+            self.addArrangedSubview(imageView)
+            self.starImages.append(imageView)
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /// Set starts rating
+    func setStarsRating(rating: Int){
+        for i in 0..<5 {
+            if i+1 > rating {
+                starImages[i].image = UIImage(systemName: starsEmptyPicName)
+            } else{
+                starImages[i].image = UIImage(systemName: starsFilledPicName)
+            }
+        }
+    }
+}
