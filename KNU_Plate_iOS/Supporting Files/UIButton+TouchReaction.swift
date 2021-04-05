63ed19384cf903211c7c9fcab02bb8bd251cda2f
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-04-05 17:28:33 +0900

    Add addBounceReactionWithoutFeedback

--- KNU_Plate_iOS/Supporting Files/UIButton+TouchReaction.swift
+++ KNU_Plate_iOS/Supporting Files/UIButton+TouchReaction.swift
@@ -8,6 +8,12 @@ extension UIButton {
         addTarget(self, action: #selector(touchUpSucceeded(_:)), for: .touchUpInside)
     }
     
+    func addBounceReactionWithoutFeedback() {
+        addTarget(self, action: #selector(touchDown(_:)), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(touchUpFailed(_:)), for: [.touchCancel, .touchUpOutside, .touchDragExit])
+        addTarget(self, action: #selector(touchUpSucceededWithoutFeedback(_:)), for: .touchUpInside)
+    }
+    
     @objc func touchDown(_ sender: UIButton) {
         animate {
             sender.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
@@ -24,6 +30,16 @@ extension UIButton {
         animate {
             sender.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
             UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+        } completion: { _ in
+            self.animate {
+                sender.transform = .identity
+            }
+        }
+    }
+    
+    @objc func touchUpSucceededWithoutFeedback(_ sender: UIButton) {
+        animate {
+            sender.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
         } completion: { _ in
             self.animate {
                 sender.transform = .identity
