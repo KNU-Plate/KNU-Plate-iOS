a268b5c252a50c7779114ebe5850b11759d264f3
김영채 <kevinkim2586@gmail.com>
2021-04-28 00:08:31 +0900

    Edit MVVM structure partially

---
+++ KNU_Plate_iOS/Models/RequestModel/NewMenuModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewMenuModel.swift
+//  KNU_Plate_iOS
+//
+//  Created by Kevin Kim on 2021/04/27.
+//
+
+import Foundation
--- KNU_Plate_iOS/Models/RequestModel/NewReviewModel.swift
+++ KNU_Plate_iOS/Models/RequestModel/NewReviewModel.swift
@@ -3,7 +3,7 @@ import Alamofire
 
 //MARK: - 한 매장에 대한 신규 리뷰 작성을 위한 Model
 
-class NewReview {
+class NewReviewModel {
     
     var rating: Int
 //    var reviewImages: Data?              // 데이터 타입이 Data 맞는지 확인해보기 -> byte buffer 형태로 보내는게 맞는가?
