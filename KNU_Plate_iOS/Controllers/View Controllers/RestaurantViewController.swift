b6a6b61c6f0040d961364db5e45f36e01fca7b20
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-05-05 18:55:33 +0900

    Edit RestaurantVC

--- KNU_Plate_iOS/Controllers/View Controllers/RestaurantViewController.swift
+++ KNU_Plate_iOS/Controllers/View Controllers/RestaurantViewController.swift
@@ -2,21 +2,42 @@ import UIKit
 
 class RestaurantViewController: UIViewController {
 
+    let stackView1: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 10
+//        stackView.isLayoutMarginsRelativeArrangement = true
+//        stackView.layoutMargins.left = 30
+//        stackView.layoutMargins.right = 30
+        return stackView
+    }()
+    
+    let nameLabel: UILabel = UILabel()
+    let favoriteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "favorite tab bar icon"), for: .normal)
+//        button.setImage(UIImage(named: "favorite tab bar icon(filled)"), for: .selected)
+        button.addBounceReactionWithoutFeedback()
+        return button
+    }()
+    let nameLabel2: UILabel = UILabel()
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let safeArea = self.view.safeAreaLayoutGuide
+        nameLabel.text = "반미리코"
+        nameLabel2.text = "끄악"
+        stackView1.addArrangedSubview(nameLabel)
+        stackView1.addArrangedSubview(favoriteButton)
+        stackView1.addArrangedSubview(nameLabel2)
+        self.view.addSubview(stackView1)
+        stackView1.snp.makeConstraints { make in
+            make.top.equalTo(safeArea.snp.top)
+            make.left.right.equalToSuperview()
+        }
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
