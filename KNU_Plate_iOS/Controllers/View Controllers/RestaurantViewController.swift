305fe9edb60730ebc43e9412f3fc182298d600cb
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-05-06 03:02:50 +0900

    Implement RestaurantView

--- KNU_Plate_iOS/Controllers/View Controllers/RestaurantViewController.swift
+++ KNU_Plate_iOS/Controllers/View Controllers/RestaurantViewController.swift
@@ -1,43 +1,24 @@
 import UIKit
+import SnapKit
 
 class RestaurantViewController: UIViewController {
 
-    let stackView1: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.alignment = .fill
-        stackView.spacing = 10
-//        stackView.isLayoutMarginsRelativeArrangement = true
-//        stackView.layoutMargins.left = 30
-//        stackView.layoutMargins.right = 30
-        return stackView
-    }()
-    
-    let nameLabel: UILabel = UILabel()
-    let favoriteButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "favorite tab bar icon"), for: .normal)
-//        button.setImage(UIImage(named: "favorite tab bar icon(filled)"), for: .selected)
-        button.addBounceReactionWithoutFeedback()
-        return button
-    }()
-    let nameLabel2: UILabel = UILabel()
+    let restaurantView = RestaurantView()
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(restaurantView)
         let safeArea = self.view.safeAreaLayoutGuide
-        nameLabel.text = "반미리코"
-        nameLabel2.text = "끄악"
-        stackView1.addArrangedSubview(nameLabel)
-        stackView1.addArrangedSubview(favoriteButton)
-        stackView1.addArrangedSubview(nameLabel2)
-        self.view.addSubview(stackView1)
-        stackView1.snp.makeConstraints { make in
-            make.top.equalTo(safeArea.snp.top)
+        restaurantView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(safeArea)
             make.left.right.equalToSuperview()
         }
-        // Do any additional setup after loading the view.
+        
+        restaurantView.nameLabel.text = "반미리코"
+        restaurantView.gateNameLabel.text = "북문"
+        restaurantView.ratingStackView.setAverageRating(rating: 4.7)
+        restaurantView.foodCategoryLabel.text = "세계 음식"
+        restaurantView.numberLabel.text = "\(39)명 참여"
     }
 }
--- KNU_Plate_iOS/Views/RestaurantView.swift
+++ KNU_Plate_iOS/Views/RestaurantView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Then
 
 class RestaurantView: UIView {
     
@@ -6,16 +7,157 @@ class RestaurantView: UIView {
     
     let imageContentsView = UIView()
     
-    let imageView1 = UIImageView()
-    let imageView2 = UIImageView()
-    let imageView3 = UIImageView()
-    let imageView4 = UIImageView()
+    let imageButton1 = UIButton().then {
+        $0.backgroundColor = UIColor.red
+    }
+    let imageButton2 = UIButton().then {
+        $0.backgroundColor = UIColor.blue
+    }
+    let imageButton3 = UIButton().then {
+        $0.backgroundColor = UIColor.yellow
+    }
+    let imageButton4 = UIButton().then {
+        $0.backgroundColor = UIColor.green
+        $0.alpha = 0.5
+        $0.setTitle("이미지 더보기", for: .normal)
+        $0.setTitleColor(UIColor.white, for: .normal)
+    }
     
-    let stackView1 = UIStackView()
+    let stackView1 = UIStackView().then {
+        $0.axis = .horizontal
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
     
+    let nameLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 25)
+    }
+    let favoriteButton = UIButton().then {
+        $0.setImage(UIImage(named: "favorite tab bar icon"), for: .normal)
+//        $0.setImage(UIImage(named: "favorite tab bar icon(filled)"), for: .selected)
+        $0.addBounceReactionWithoutFeedback()
+    }
+    
+    let stackView2 = UIStackView().then {
+        $0.axis = .horizontal
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    
+    let gateNameLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 20)
+    }
+    let ratingStackView = RatingStackView()
+    
+    let stackView3 = UIStackView().then {
+        $0.axis = .horizontal
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    
+    let foodCategoryLabel = UILabel().then {
+        $0.textColor = UIColor.lightGray
+        $0.font = UIFont.systemFont(ofSize: 20)
+    }
+    let numberLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        imageContentsView.addSubview(imageButton1)
+        imageContentsView.addSubview(imageButton2)
+        imageContentsView.addSubview(imageButton3)
+        imageContentsView.addSubview(imageButton4)
+
+        stackView1.addArrangedSubview(nameLabel)
+        stackView1.addArrangedSubview(favoriteButton)
+
+        stackView2.addArrangedSubview(gateNameLabel)
+        stackView2.addArrangedSubview(ratingStackView)
+
+        stackView3.addArrangedSubview(foodCategoryLabel)
+        stackView3.addArrangedSubview(numberLabel)
+
+        scrollView.addSubview(imageContentsView)
+        scrollView.addSubview(stackView1)
+        scrollView.addSubview(stackView2)
+        scrollView.addSubview(stackView3)
+
+        self.addSubview(scrollView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        let imageContentsViewHeight: CGFloat = frame.height*0.4
+        let imageContentsViewWidth: CGFloat = frame.width
+
+        imageContentsView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(imageContentsViewHeight)
+            make.width.equalTo(imageContentsViewWidth)
+        }
+
+        let padding: CGFloat = 3
+        let paddingSpace: CGFloat = padding*3
+        let imageButtonHeight: CGFloat = (imageContentsViewHeight-paddingSpace)/2
+        let imageButtonWidth: CGFloat = (imageContentsViewWidth-paddingSpace)/2
+
+        imageButton1.snp.makeConstraints { make in
+            make.height.equalTo(imageButtonHeight)
+            make.width.equalTo(imageButtonWidth)
+            make.top.left.equalToSuperview().inset(padding)
+            make.right.equalTo(imageButton2.snp.left).offset(-padding)
+        }
+
+        imageButton2.snp.makeConstraints { make in
+            make.height.equalTo(imageButtonHeight)
+            make.width.equalTo(imageButtonWidth)
+            make.top.right.equalToSuperview().inset(padding)
+        }
+
+        imageButton3.snp.makeConstraints { make in
+            make.height.equalTo(imageButtonHeight)
+            make.width.equalTo(imageButtonWidth)
+            make.top.equalTo(imageButton1.snp.bottom).offset(padding)
+            make.left.bottom.equalToSuperview().inset(padding)
+            make.right.equalTo(imageButton4.snp.left).offset(-padding)
+        }
+
+        imageButton4.snp.makeConstraints { make in
+            make.height.equalTo(imageButtonHeight)
+            make.width.equalTo(imageButtonWidth)
+            make.top.equalTo(imageButton2.snp.bottom).offset(padding)
+            make.right.bottom.equalToSuperview().inset(padding)
+        }
+        
+//        let stackViewHeight: CGFloat = frame.height*0.1
+
+        stackView1.snp.makeConstraints { make in
+//            make.height.equalTo(stackViewHeight)
+            make.top.equalTo(imageContentsView.snp.bottom).offset(padding*2)
+            make.left.right.equalToSuperview()
+        }
+
+        stackView2.snp.makeConstraints { make in
+//            make.height.equalTo(stackViewHeight)
+            make.top.equalTo(stackView1.snp.bottom).offset(padding*2)
+            make.left.right.equalToSuperview()
+        }
+        
+        ratingStackView.snp.makeConstraints { make in
+            make.width.equalTo(frame.width/6)
+        }
+
+        stackView3.snp.makeConstraints { make in
+//            make.height.equalTo(stackViewHeight)
+            make.top.equalTo(stackView2.snp.bottom).offset(padding*2)
+            make.left.right.equalToSuperview()
+        }
     }
     
     required init?(coder: NSCoder) {
