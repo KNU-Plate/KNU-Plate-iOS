3e78c706dd997bc821eebe0c9c5a50ebb4c3754b
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-05-11 23:42:29 +0900

    Add ReviewVC, LocationVC, MenuVC

---
+++ KNU_Plate_iOS/Controllers/View Controllers/LocationViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import SnapKit
+
+class LocationViewController: UIViewController, MTMapViewDelegate {
+
+    let mapView = MTMapView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.baseMapType = .standard
+        self.view.addSubview(mapView)
+        
+        mapView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(250)
+        }
+    }
+}
