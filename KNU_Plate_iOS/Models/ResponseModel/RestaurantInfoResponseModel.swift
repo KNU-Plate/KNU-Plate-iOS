6698b396cbec23348e9ea351458b15749e7f1bf0
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-07-07 15:58:18 +0900

    Add files related to RestaurantInfoVC

---
+++ KNU_Plate_iOS/Models/ResponseModel/RestaurantInfoResponseModel.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct RestaurantInfoResponseModel: Decodable {
+    let mallID: Int
+    let userID: String
+    let dateCreated: TimeInterval
+    let mallName: String
+    let contact: String?
+    let categoryName: String?
+    let address: String?
+    let latitude: Double?
+    let longitude: Double?
+    let thumbnail: String?
+    let evaluateAverage: Double?
+    let recommendCount: Int
+    let isActive: String?
+    let gateLocation: String?
+    let fileFolder: FileFolder?
+    let menus: [ExistingMenuModel]
+    
+    enum CodingKeys: String, CodingKey {
+        case mallID = "mall_id"
+        case userID = "user_id"
+        case dateCreated = "date_create"
+        case mallName = "mall_name"
+        case contact
+        case categoryName = "category_name"
+        case address, latitude, longitude, thumbnail
+        case evaluateAverage = "evaluate_average"
+        case recommendCount = "recommend_count"
+        case isActive = "is_active"
+        case gateLocation = "gate_location"
+        case fileFolder = "file_folder"
+        case menus
+    }
+}
