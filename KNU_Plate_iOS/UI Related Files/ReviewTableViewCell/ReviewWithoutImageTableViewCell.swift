3509abb3ecc28bc5ab0794e889f6af91fce2ac5f
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-07-16 13:43:07 +0900

    Fix error when merged

--- KNU_Plate_iOS/UI Related Files/ReviewTableViewCell/ReviewWithoutImageTableViewCell.swift
+++ KNU_Plate_iOS/UI Related Files/ReviewTableViewCell/ReviewWithoutImageTableViewCell.swift
@@ -24,51 +24,21 @@ class ReviewWithoutImageTableViewCell: ReviewTableViewCell {
                                             message: nil,
                                             preferredStyle: .actionSheet)
         
-        print("✏️ viewModel.usernickname: \(viewModel.userNickname)")
-        print("✏️ user.shared.displayname: \(User.shared.displayName)")
-        
-        if viewModel.userNickname == User.shared.displayName {
+        let reportReview = UIAlertAction(title: "게시글 신고하기",
+                                         style: .default) { alert in
             
-            let deleteAction = UIAlertAction(title: "리뷰 삭제하기",
-                                             style: .destructive) { onPress in
-                
-                self.delegate?.presentDeleteActionAlert(reviewID: self.viewModel.reviewID)
-            }
-            actionSheet.addAction(deleteAction)
-        }
-        else {
-            let reportAction = UIAlertAction(title: "게시글 신고하기",
-                                             style: .default) { onPress in
-                self.delegate?.goToReportReviewVC(reviewID: self.viewModel.reviewID,
-                                                  displayName: self.viewModel.userNickname)
-            }
-            actionSheet.addAction(reportAction)
+            self.delegate?.goToReportReviewVC(reviewID: self.reviewID,
+                                              displayName: self.userNickname)
         }
         
         let cancelAction = UIAlertAction(title: "취소",
                                          style: .cancel,
                                          handler: nil)
+        actionSheet.addAction(reportReview)
         actionSheet.addAction(cancelAction)
         
         let vc = self.window?.rootViewController
         vc?.present(actionSheet, animated: true)
-    }
-    override func getReviewDetails() -> ReviewDetail {
-
-        let profileImage = userProfileImageView.image ?? UIImage(named: Constants.Images.defaultProfileImage)!
-        let nickname = viewModel.userNickname
-        let medal = viewModel.medal
-        let rating = viewModel.rating
-        let review = viewModel.review
-
-        let reviewDetails = ReviewDetail(profileImage: profileImage,
-                                         nickname: nickname,
-                                         medal: medal,
-                                         reviewImageFiles: nil,
-                                         rating: rating,
-                                         review: review)
-        return reviewDetails
-
     }
 }
 
