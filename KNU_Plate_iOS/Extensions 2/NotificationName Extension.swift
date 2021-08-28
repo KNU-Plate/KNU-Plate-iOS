2ea51b685ac2df5c3a2ecb91403b41e89926b499
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-08-28 15:49:24 +0900

    Merge

---
+++ KNU_Plate_iOS/Extensions 2/NotificationName Extension.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Notification.Name {
+
+    static let presentWelcomeVC = Notification.Name("co.kuchelin.presentWelcomeVC")
+    static let refreshTokenExpired = Notification.Name("co.kuchelin.refreshTokenExpired")
+
+    static let didMarkFavorite = Notification.Name("co.kuchelin.didMarkFavorite")
+    static let didFailedMarkFavorite = Notification.Name("co.kuchelin.didFailedMarkFavorite")
+}
